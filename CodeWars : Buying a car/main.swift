c8099d2a0e715627eb6f48c5ff55214cd67c998e
Tanatip Denduangchai <De.tanatip@outlook.com>
2019-08-20 11:38:14 +0700

    Initial Commit

---
+++ CodeWars : Buying a car/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeWars : Buying a car
+//
+//  Created by Tanatip Denduangchai on 8/20/19.
+//  Copyright © 2019 Tanatip Denduangchai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
